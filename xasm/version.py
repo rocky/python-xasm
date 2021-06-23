442f7deff20c7e7643faa94f06f633f99ffd9d10
rocky <rocky@dusytfeet.com>
2021-06-23 11:51:56 -0400

    Black shouldn't format version.py

--- xasm/version.py
+++ xasm/version.py
@@ -3,4 +3,6 @@
 # This file is suitable for sourcing inside POSIX shell as
 # well as importing into Python. That's why there is no
 # space around "=" below.
-__version__="1.1.1"  # noqa
+
+# fmt: off
+__version__="1.1.2.dev0"  # noqa
