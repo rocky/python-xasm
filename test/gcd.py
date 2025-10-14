da77b3657002b7f4fa68ce7723c6c7adf30f5904
rocky <rb@dusytfeet.com>
2025-10-13 20:31:05 -0400

    Get ready for release 1.2.1

--- test/gcd.py
+++ test/gcd.py
@@ -8,21 +8,24 @@ check_args() raises an uncaught exception when given the wrong number
 of parameters.
 
 """
-def gcd(a,b):
-    """ GCD. We assume positive numbers"""
+
+
+def gcd(a: int, b: int):
+    """GCD. We assume positive numbers"""
 
     # Make: a <= b
     if a > b:
-       (a, b) = (b, a)
-       pass
+        (a, b) = (b, a)
+        pass
 
     if a <= 0:
         return None
     if a == 1 or a == b:
         return a
-    return gcd(b-a, a)
+    return gcd(b - a, a)
 
-if __name__=='__main__':
+
+if __name__ == "__main__":
     a, b = 3, 5
     print("The GCD of %d and %d is %d" % (a, b, gcd(a, b)))
     pass
